06bdb88bb766fdbef8754c5a516c8eb625042311
JaZahn <jcleveng@fas.harvard.edu>
2015-08-01 23:35:26 -0400

    first commit

---
+++ js/app.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+  $('#zombie-to-english-btn').click(function(event){
+
+    return false;
+  });
+
+  $('#english-to-zombie-btn').click(function(event){
+
+    return false;
+  });
+
+  function zombify(){
+    // 1. lower-case "r" at the end of words replaced with "rh".
+    // 2. an "a" or "A" by itself will be replaced with "hra".
+    // 3. the starts of sentences are capitalised (the "start of a sentence" is any occurrence of
+    //   ".!?", followed by a space, followed by a letter.)
+    // 4. "e" or "E" is replaced by "rr"
+    // 5. "i" or "I" is replaced by "rrRr"
+    // 6. "o" or "O" is replaced by "rrrRr"
+    // 7. "u" or "U" is replaced by "rrrrRr"
+    // 8. "r" or "R' is replaced by "RR"
+  }
+
+  function unzombify(){
+
+
+  }
+
+});
