1a69a85712d7e1e15980e065e27fc097c90e9a0e
JaZahn <jcleveng@fas.harvard.edu>
2015-09-09 14:20:31 -0400

    labels

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
   $('#zombie-to-english-btn').click(function(event){
-
+    unzombify();
     return false;
   });
 
   $('#english-to-zombie-btn').click(function(event){
-
+    zombify();
     return false;
   });
 
